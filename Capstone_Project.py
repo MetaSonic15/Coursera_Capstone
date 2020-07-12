7bac58c64d104209fe01be36c4691bfae1b92ca0
MetaSonic15 <kanishkramagiri47@gmail.com>
2020-07-12 14:35:25 +0530

    python script for the capstone project

---
+++ Capstone_Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> CAPSTONE PROJECT </h1>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
